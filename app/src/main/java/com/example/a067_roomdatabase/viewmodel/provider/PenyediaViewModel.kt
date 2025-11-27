2a9100f09cce13febddd949eaaf21d887e7e6238
Naufal0067 <naufalopang1928@gmail.com>
2025-11-27 14:06:27 +0700

    menambahkan fun creationextras

--- app/src/main/java/com/example/a067_roomdatabase/viewmodel/provider/PenyediaViewModel.kt
+++ app/src/main/java/com/example/a067_roomdatabase/viewmodel/provider/PenyediaViewModel.kt
@@ -1,7 +1,10 @@
 package com.example.a067_roomdatabase.viewmodel.provider
 
+import androidx.lifecycle.ViewModelProvider
+import androidx.lifecycle.viewmodel.CreationExtras
 import androidx.lifecycle.viewmodel.initializer
 import androidx.lifecycle.viewmodel.viewModelFactory
+import com.example.a067_roomdatabase.repositori.AplikasiSiswa
 import com.example.a067_roomdatabase.viewmodel.EntryViewModel
 import com.example.a067_roomdatabase.viewmodel.HomeViewModel
 
@@ -15,3 +18,11 @@ object PenyediaViewModel{
         }
     }
 }
+
+/**
+ * Fungsi ekstensi query untuk objek Application dan mengembalikan sebuah instance dari
+ * AplikasiSiswa
+ */
+
+fun CreationExtras.aplikasiSiswa(): AplikasiSiswa =
+    (this[ViewModelProvider.AndroidViewModelFactory.APPLICATION_KEY] as AplikasiSiswa)
