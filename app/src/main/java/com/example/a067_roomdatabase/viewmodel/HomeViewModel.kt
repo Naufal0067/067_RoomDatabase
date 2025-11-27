023a1a7adc17aff8ed09fdcbb48e383e437c09b6
Naufal0067 <naufalopang1928@gmail.com>
2025-11-27 14:02:29 +0700

    menambahkan import

--- app/src/main/java/com/example/a067_roomdatabase/viewmodel/HomeViewModel.kt
+++ app/src/main/java/com/example/a067_roomdatabase/viewmodel/HomeViewModel.kt
@@ -1,7 +1,14 @@
 package com.example.a067_roomdatabase.viewmodel
 
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
 import com.example.a067_roomdatabase.repositori.RepositoriSiswa
+import com.example.a067_roomdatabase.room.Siswa
+import kotlinx.coroutines.flow.SharingStarted
+import kotlinx.coroutines.flow.StateFlow
+import kotlinx.coroutines.flow.filterNotNull
+import kotlinx.coroutines.flow.map
+import kotlinx.coroutines.flow.stateIn
 
 class HomeViewModel(private val repositoriSiswa: RepositoriSiswa): ViewModel() {
     companion object {
