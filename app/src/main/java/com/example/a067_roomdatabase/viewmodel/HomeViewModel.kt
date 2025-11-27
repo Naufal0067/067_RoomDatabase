54510b1e5d8ad3adf13c9cd716078b3844e803b0
Naufal0067 <naufalopang1928@gmail.com>
2025-11-27 13:59:09 +0700

    menambah companion object

--- app/src/main/java/com/example/a067_roomdatabase/viewmodel/HomeViewModel.kt
+++ app/src/main/java/com/example/a067_roomdatabase/viewmodel/HomeViewModel.kt
@@ -4,5 +4,7 @@ import androidx.lifecycle.ViewModel
 import com.example.a067_roomdatabase.repositori.RepositoriSiswa
 
 class HomeViewModel(private val repositoriSiswa: RepositoriSiswa): ViewModel() {
-
+    companion object {
+        private const val TIMEOUT_MILLIS = 5_000L
+    }
 }
