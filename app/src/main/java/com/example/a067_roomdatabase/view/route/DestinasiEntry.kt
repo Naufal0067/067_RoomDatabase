e870bb011a64a605aafc18161f7882169d2cc969
Naufal0067 <naufalopang1928@gmail.com>
2025-11-27 14:30:03 +0700

    menambahkan code object di DestinasiEntry.kt

---
+++ app/src/main/java/com/example/a067_roomdatabase/view/route/DestinasiEntry.kt
@@ -0,0 +1,8 @@
+package com.example.a067_roomdatabase.view.route
+
+import com.example.a067_roomdatabase.R
+
+object DestinasiEntry : DestinasiNavigasi {
+    override val route = "home"
+    override val titleRes = R.string.entry_siswa
+}
