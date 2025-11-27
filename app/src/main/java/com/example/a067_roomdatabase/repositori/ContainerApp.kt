6624942b5fbcb1877f9aa03598a340514a2069e5
Naufal0067 <naufalopang1928@gmail.com>
2025-11-27 14:31:15 +0700

    menambahkan class aplikasi dab containerdataapp

--- app/src/main/java/com/example/a067_roomdatabase/repositori/ContainerApp.kt
+++ app/src/main/java/com/example/a067_roomdatabase/repositori/ContainerApp.kt
@@ -1,2 +1,28 @@
 package com.example.a067_roomdatabase.repositori
 
+import android.app.Application
+import android.content.Context
+import com.example.a067_roomdatabase.room.DatabaseSiswa
+
+interface ContainerApp {
+    val repositoriSiswa : RepositoriSiswa
+}
+
+class ContainerDataApp(private val context: Context):
+        ContainerApp {
+            override val repositoriSiswa: RepositoriSiswa by lazy {
+                OfflineRepositoriSiswa(
+                    DatabaseSiswa.getDatabase(context).siswaDao()
+                )
+            }
+        }
+
+class AplikasiSiswa : Application() {
+    /*** AppController instance digunakan oleh kelas kelas lainnya untuk mendapatkan dependensi*/
+    lateinit var container: ContainerApp
+
+    override fun onCreate() {
+        super.onCreate()
+        container = ContainerDataApp(this)
+    }
+}
