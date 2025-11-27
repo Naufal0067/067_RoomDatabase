2ae5ef6efee4b44eaa28f7352dd8a3a9909ccff9
Naufal0067 <naufalopang1928@gmail.com>
2025-11-27 13:47:14 +0700

    menambahkan private fun validasi input

--- app/src/main/java/com/example/a067_roomdatabase/viewmodel/EntryViewModel.kt
+++ app/src/main/java/com/example/a067_roomdatabase/viewmodel/EntryViewModel.kt
@@ -1,2 +1,21 @@
 package com.example.a067_roomdatabase.viewmodel
 
+import androidx.compose.runtime.State
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.setValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.lifecycle.ViewModel
+import com.example.a067_roomdatabase.repositori.RepositoriSiswa
+
+class EntryViewModel(private val repositoriSiswa: RepositoriSiswa): ViewModel() {
+    /** berisi status siswa saat ini */
+    var uiStateSiswa by mutableStateOf(UIStateSiswa())
+        private set
+
+    private fun validasiInput(
+        uiState: DetailSiswa = uiStateSiswa
+            .detailSiswa
+    ): Boolean {
+        nama.isNotBlank() && alamat.isNotBlank() && telepon.isNotBlank()
+    }
+}
