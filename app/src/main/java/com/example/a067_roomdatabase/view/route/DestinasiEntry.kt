dbff23a383b586f1748bb1dcac47ac10e8f80c4d
Naufal0067 <naufalopang1928@gmail.com>
2025-11-27 15:28:42 +0700

    memperbaiki code yang salah

--- app/src/main/java/com/example/a067_roomdatabase/view/route/DestinasiEntry.kt
+++ app/src/main/java/com/example/a067_roomdatabase/view/route/DestinasiEntry.kt
@@ -3,6 +3,6 @@ package com.example.a067_roomdatabase.view.route
 import com.example.a067_roomdatabase.R
 
 object DestinasiEntry : DestinasiNavigasi {
-    override val route = "home"
+    override val route = "item_entry"
     override val titleRes = R.string.entry_siswa
 }
