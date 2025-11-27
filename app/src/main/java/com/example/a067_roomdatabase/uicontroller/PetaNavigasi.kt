a9692bdc73c1f1e95d411e2053ffce047a559658
Naufal0067 <naufalopang1928@gmail.com>
2025-11-27 14:25:50 +0700

    menambahkan code fun hostnavigasi

---
+++ app/src/main/java/com/example/a067_roomdatabase/uicontroller/PetaNavigasi.kt
@@ -0,0 +1,26 @@
+package com.example.a067_roomdatabase.uicontroller
+
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.navigation.NavHostController
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+import androidx.navigation.compose.rememberNavController
+import com.example.a067_roomdatabase.view.EntrySiswaScreen
+import com.example.a067_roomdatabase.view.HomeScreen
+import com.example.a067_roomdatabase.view.route.DestinasiEntry
+import com.example.a067_roomdatabase.view.route.DestinasiHome
+
+
+@Composable
+fun SiswaApp(navController: NavHostController= rememberNavController(), modifier: Modifier){
+    HostNavigasi(navController = navController)
+}
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun HostNavigasi(
+    navController: NavHostController,
+    modifier: Modifier = Modifier
+)
