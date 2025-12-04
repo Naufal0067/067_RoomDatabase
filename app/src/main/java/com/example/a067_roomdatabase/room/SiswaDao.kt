3ba41d1218b8246947332d80e62447460cc616b9
Naufal0067 <naufalopang1928@gmail.com>
2025-12-04 11:30:00 +0700

    menambahkan fun get siswa id

--- app/src/main/java/com/example/a067_roomdatabase/room/SiswaDao.kt
+++ app/src/main/java/com/example/a067_roomdatabase/room/SiswaDao.kt
@@ -14,4 +14,8 @@ interface SiswaDao {
     @Insert(onConflict = OnConflictStrategy.IGNORE)
     suspend fun insert(siswa: Siswa)
 
+    //edit : tambah 3 fungsi berikut
+    @Query("SELECT * from tblSiswa WHERE id = :id")
+    fun getSiswa(id: Int): Flow<Siswa>
+
 }
