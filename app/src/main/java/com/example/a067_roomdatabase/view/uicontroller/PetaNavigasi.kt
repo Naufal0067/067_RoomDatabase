fae15ebb8d65b09aedc7657fa2ea5040bc477f4e
Naufal0067 <naufalopang1928@gmail.com>
2025-12-04 12:15:00 +0700

    Menambahkan composable DetailSiswa dengan argumen ID bertipe Int

--- app/src/main/java/com/example/a067_roomdatabase/view/uicontroller/PetaNavigasi.kt
+++ app/src/main/java/com/example/a067_roomdatabase/view/uicontroller/PetaNavigasi.kt
@@ -4,12 +4,16 @@ import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.navigation.NavHostController
+import androidx.navigation.NavType
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
 import androidx.navigation.compose.rememberNavController
+import androidx.navigation.navArgument
+import com.example.a067_roomdatabase.view.DetailSiswaScreen
 import com.example.a067_roomdatabase.view.EntrySiswaScreen
 import com.example.a067_roomdatabase.view.HomeScreen
 import com.example.a067_roomdatabase.view.route.DestinasiDetailSiswa
+import com.example.a067_roomdatabase.view.route.DestinasiDetailSiswa.itemIdArg
 import com.example.a067_roomdatabase.view.route.DestinasiEntry
 import com.example.a067_roomdatabase.view.route.DestinasiHome
 
@@ -39,6 +43,12 @@ fun HostNavigasi(
         composable(DestinasiEntry.route){
             EntrySiswaScreen(navigateBack = { navController.popBackStack()})
         }
+
+        composable(route = DestinasiDetailSiswa.routeWithArgs,
+            arguments = listOf(navArgument(itemIdArg) {
+                type = NavType.IntType
+            })
+        )
 
     }
 }
